facb0d52d024004d0000ca48ff8013f73512fb7f
Nick Loomis <nloomis@gmail.com>
2018-08-17 22:44:09 -0700

    Red sphere added to trace

--- ray_tracer/trace.cc
+++ ray_tracer/trace.cc
@@ -5,7 +5,20 @@
 
 namespace trace {
 
+bool HitSphere(const Eigen::Vector3d& center, double radius, const Ray& ray) {
+  Eigen::Vector3d relative_center = ray.origin() - center;
+  double a_coefficient = ray.direction().dot(ray.direction());
+  double b_coefficient = 2.0 * relative_center.dot(ray.direction());
+  double c_coefficient = relative_center.squaredNorm() - radius * radius;
+  double discriminant = b_coefficient * b_coefficient - 
+      4 * a_coefficient * c_coefficient;
+  return discriminant > 0;
+}
+
 Eigen::Vector3d color(const Ray& ray) {
+  if (HitSphere(Eigen::Vector3d(0, 0, -1), 0.5, ray)) {
+    return Eigen::Vector3d(1, 0, 0);
+  }
   Eigen::Vector3d unit_direction = ray.direction().normalized();
   double t = 0.5 * (unit_direction.y() + 1);
   // LERP between white and blue depending on the y component of the ray's
@@ -26,8 +39,8 @@ int main() {
   for (int j = ny - 1; j >= 0; --j) {
     for (int i = 0; i < nx; ++i) {
       // (u, v) parameterizes the canvas and ranges from [0, 1] (ish).
-      double u = i / static_cast<double>(nx) + 0.5;
-      double v = j / static_cast<double>(ny) + 0.5;
+      double u = (i + 0.5) / static_cast<double>(nx);
+      double v = (j + 0.5) / static_cast<double>(ny);
       Eigen::Vector3d ray_direction = lower_left_corner - camera_origin + 
           u * canvas_width + v * canvas_height;
       trace::Ray ray(camera_origin, ray_direction);
